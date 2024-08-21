b96796b2e618f2b461871af368b89bc19a0d8b05
Lee Jihee <129031670+munjji@users.noreply.github.com>
2024-08-22 00:19:35 +0900

    [INIT] django 초기 세팅

---
+++ lotto/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def index(request):
+
+    # user_input_name = request.POST['name']
+    # user_input_text = request.POST['text']
+
+    # new_row =  GuessNumbers(name=user_input_name, text = user_input_text)
+    
+    # print(new_row.num_lotto)
+    # print(new_row.name)
+
+    # new_row.name = new_row.name.upper()
+    # new_row.lottos = [np.randint(1, 50) for i in range(6)]
+
+    # new_row.save()
+    
+    return HttpResponse('<h1>hi im jihee</h1>')
+
+
+def hello (request):
+    return HttpResponse("<h1 style='color:blue;'>Hello!</h1>")
