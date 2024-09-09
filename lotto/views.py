fecba9546aba1cf5b1fa038621ecea56cb9ba309
Lee Jihee <129031670+munjji@users.noreply.github.com>
2024-09-09 09:33:03 +0900

    [FEAT] lotto html props 수정

--- lotto/views.py
+++ lotto/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import GuessNumbers
 
 # Create your views here.
 def index(request):
-    return render(request, 'lotto/default.html', {})
+
+    lottos = GuessNumbers.objects.all()
+    return render(request, 'lotto/default.html', {'lottos' : lottos}) # context-dict
 
 def hello (request):
     return HttpResponse("<h1 style='color:blue;'>Hello!</h1>")
